f53d218d9ae262936a684fb2b89d8627c5ca2ea3
Matthieu-ui <mattfelker206@outlook.com>
2023-06-29 22:53:32 -0700

    update

--- src/components/react/Gallery.jsx
+++ src/components/react/Gallery.jsx
@@ -1,44 +1,39 @@
 import React from "react";
-
+import { motion } from 'framer-motion';
 
 
 const Gallery = () => {
 
     return (
         <>
-            <div className="grid grid-cols-3 gap-4  mx-auto   ">
+            <ul className="grid grid-cols-3  gap-4 w-130  mx-auto text-center">
 
-                <div className="rounded-lg drop-shadow-lg  p-5 overflow-hidden hover:scale-105 duration-300 ease-out mx-auto bg-bgColor">
+            <li className="rounded-lg w-1/2 drop-shadow-lg overflow-hidden hover:scale-105 duration-300 ease-out mx-auto bg-bgColor">
 
-                    <div className="p-4 ">
-                        <img src="/icon.png" className="mx-auto" alt="iso" />
-                       
-                    </div>
-                    <p className="font-mono">Motion Graphics</p>
-                </div>
+            <img src="/homeIcon.svg" className="mx-auto  dark:-hue-rotate-[130deg] -hue-rotate-[330deg] drop-shadow-md brightness-[150%] dark:brightness-[400%] stroke-1 " alt="iso" />
+            
+            <p className="font-mono">Icons/Logos</p>
+            </li>
+            
+            <li className="rounded-lg w-1/2 drop-shadow-lg overflow-hidden hover:scale-105 duration-300 ease-out mx-auto bg-bgColor">
 
-                <div className="rounded-lg drop-shadow-lg p-5 overflow-hidden hover:scale-105 duration-300 ease-out mx-auto bg-bgColor">
-                <img src="/homeIcon.svg" className="mx-auto grayscale dark:-hue-rotate-0 -hue-rotate-60 drop-shadow-md brightness-[150%] dark:brightness-[400%] stroke-1 stroke-black" alt="iso" />
-                <div className="p-4 ">
-                
-           
-            </div>
-            <p className="font-mono">Icons/Library</p>
-            </div>
-            <div className="rounded-lg drop-shadow-lg p-5 overflow-hidden hover:scale-105 duration-300 ease-out mx-auto bg-bgColor">
+            <img src="/homeIcon.svg" className="mx-auto  dark:-hue-rotate-[130deg] -hue-rotate-[330deg] drop-shadow-md brightness-[150%] dark:brightness-[400%] stroke-1 " alt="iso" />
+            
+            <p className="font-mono">Motion Graphics</p>
+            </li>
+            
+            <li className="rounded-lg w-1/2 drop-shadow-lg  overflow-hidden hover:scale-105 duration-300 ease-out mx-auto bg-bgColor">
 
-            <div className="p-4 ">
-          <img src="https://api.iconify.design/iconoir:www.svg" className="mx-auto w-[70px] drop-shadow-lg dark:invert" alt="iso" />
-     
-        </div>
-        <p className="font-mono">More Samples</p>
-        </div>
-            </div>
+            <img src="/homeIcon.svg" className="mx-auto grayscale dark:-hue-rotate-0 -hue-rotate-60 drop-shadow-md brightness-[150%] dark:brightness-[400%] stroke-1 " alt="iso" />
+            
+            <p className="font-mono">Web Design</p>
+            </li>
+            
+            
+          
 
-
-
+            </ul>
         </>
-
     );
 };
 
