22fe5bcdaabf89ac5e9089bf5a8d58ecd7ebb665
Matthieu-ui <mattfelker206@outlook.com>
2023-07-25 15:48:57 -0700

    theme correct

--- src/components/react/Gallery.jsx
+++ src/components/react/Gallery.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Icon } from 'astro-icon';
 
 
 
@@ -8,22 +9,23 @@ const Gallery = () => {
         <>
             <ul className="grid grid-cols-3  gap-4 w-130  mx-auto text-center">
 
-            <li className="mx-auto rounded-lg w-1/2 drop-shadow-lg overflow-hidden hover:scale-105 duration-300 group relative bg-zinc-200 ring-zinc-400 transition-all hover:ring-2 dark:bg-zinc-700 ">
+            <li className="mx-auto rounded-lg w-full drop-shadow-lg overflow-hidden duration-300 group relative border border-accent  bg-zinc-200 p-2 ring-accent transition-all hover:ring-2 dark:bg-zinc-900">
 
             <img src="/homeIcon.svg" className="mx-auto dark:-hue-rotate-[130deg] -hue-rotate-[330deg] drop-shadow-md brightness-[150%] dark:brightness-[400%] stroke-1 " alt="iso" />
             
             <p className="font-mono">Icons/Logos</p>
             </li>
             
-            <li className="mx-auto rounded-lg w-1/2 drop-shadow-lg overflow-hidden hover:scale-105 duration-300 group relative bg-zinc-200 ring-zinc-400 transition-all hover:ring-2 dark:bg-zinc-700 ">
+            <li className="mx-auto rounded-lg w-full drop-shadow-lg overflow-hidden duration-300 group relative border border-accent  bg-zinc-200 p-2 ring-accent transition-all hover:ring-2 dark:bg-zinc-900">
 
             <img src="/homeIcon.svg" className="mx-auto  dark:-hue-rotate-[130deg] -hue-rotate-[330deg] drop-shadow-md brightness-[150%] dark:brightness-[400%] stroke-1 " alt="iso" />
             
             <p className="font-mono">Motion Graphics</p>
             </li>
             
-            <li className="mx-auto rounded-lg w-1/2 drop-shadow-lg overflow-hidden hover:scale-105 duration-300 group relative bg-zinc-200 ring-zinc-400 transition-all hover:ring-2 dark:bg-zinc-700 ">
 
+            <li className="mx-auto rounded-lg w-full drop-shadow-lg overflow-hidden duration-300 group relative border border-accent  bg-zinc-200 p-2 ring-accent transition-all hover:ring-2 dark:bg-zinc-900">
+            
             <img src="/homeIcon.svg" className="mx-auto grayscale dark:-hue-rotate-0 -hue-rotate-60 drop-shadow-md brightness-[150%] dark:brightness-[400%] stroke-1 " alt="iso" />
             
             <p className="font-mono">Web Design</p>
